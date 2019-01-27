ac1d5d5600c9f40d49a1ba748ac8547a9b309abc
retardedpunk <beao3002@usherbrooke.ca>
2019-01-26 19:43:16 -0500

    test

--- Alpha/Exercices/threads/target/debug/deps/threads-d5e2d51f01bbb3b2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\olibb\rust_projects\rust_playground\Alpha\Exercices\threads\target\debug\deps\threads-d5e2d51f01bbb3b2.exe: src\main.rs
-
-C:\Users\olibb\rust_projects\rust_playground\Alpha\Exercices\threads\target\debug\deps\threads-d5e2d51f01bbb3b2.d: src\main.rs
-
-src\main.rs:
--- Alpha/Exercices/threads/target/debug/threads.d
+++
@@ -1,1 +0,0 @@
-C:\Users\olibb\rust_projects\rust_playground\Alpha\Exercices\threads\target\debug\threads.pdb: C:\Users\olibb\rust_projects\rust_playground\Alpha\Exercices\threads\src\main.rs
